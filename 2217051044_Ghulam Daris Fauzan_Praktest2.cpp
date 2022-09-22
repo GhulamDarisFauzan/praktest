f197f5afcc5fc2aa8f587f49504e6b2f53e7f2bd
GhulamDarisFauzan <112993129+GhulamDarisFauzan@users.noreply.github.com>
2022-09-22 16:52:12 +0700

    Add files via upload

---
+++ 2217051044_Ghulam Daris Fauzan_Praktest2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int GDF;
+    
+    cin>>GDF;
+    
+	
+	if(GDF==1)
+	{	
+		cout<<"satu"<<endl;
+	}
+	
+	else if(GDF==2)
+	{
+		cout<<"dua"<<endl;
+	}
+	
+	else if(GDF==3)
+	{
+		cout<<"tiga"<<endl;
+	}
+	
+	else if(GDF>3)
+	{
+		cout<<"bilangan lebih besar dari tiga "<<endl;
+	}
+	
+	
+	return 0;
+}
